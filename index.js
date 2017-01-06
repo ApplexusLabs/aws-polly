3d14c9c906adcdd0bcda161d1d0c16a59246e4d8
Scott Pletcher <scott.pletcher@applexus.com>
2017-01-05 17:27:55 -0700

    refactored

--- index.js
+++ index.js
@@ -1,55 +1,89 @@
-var AWS = require('aws-sdk');
-const uuidV4 = require('uuid/v4');
-const songLibrary = require('./lyrics');
+"use strict";
 
-AWS.config.region = 'us-east-1';
+var AWS = require("aws-sdk");
+const uuidV4 = require("uuid/v4");
+const songLibrary = require("./lyrics");
+const phraseBook = require("./phrases");
+
+const lang = "en";
+
+AWS.config.region = "us-east-1";
 
 var polly = new AWS.Polly();
 var s3 = new AWS.S3();
 
 exports.handler = (event, context, callback) => {
 
+    var phrases = phraseBook.phrases.find(phrase => phrase.lang === lang);
+    var lyrics = songLibrary.lyrics[Math.floor(Math.random() * (songLibrary.lyrics.length))];
+
     var now = new Date();
 
-    var index = Math.floor(Math.random() * (songLibrary.lyrics.length))
-    var currentTime = now.getUTCHours().toString() + ":" + ("00" + now.getUTCMinutes()).substr(-2, 2);
+    var script = ""
+        + "<speak>"
+        + "<p>"
+        + phrases.greeting
+        + "</p>"
+        + "<p>"
+        + phrases.currentTimePrefix
+        + "<say-as interpret-as='time'>"
+        + now.getUTCHours() + ":" + ("00" + now.getUTCMinutes()).substr(-2, 2)
+        + "</say-as>"
+        + phrases.currentTimePostfix
+        + "</p>";
 
-    var greeting = "<p>Yeah boy ease. Its the Public Enemy, lyric line.</p>"
-    var sayTime = "<p>The current time is <say-as interpret-as='time'>" + currentTime + "</say-as>coordinated universal time.</p>";
-    var outro = "<p><break time='2s'/>We hope you have enjoyed these words of wisdom.  Thank you and good day.<break time='3s'/>  I said<break strength='weak'/>good day!</p>"
+    if (lyrics.song === "Never Gonna Give You Up"){
+        script +=
+            "<p>"
+            + phrases.prank
+            + "</p>";
+    } else {
+        script +=
+            "<p>"
+            + phrases.songTag
+            + " '" + lyrics.song + "' "
+            + phrases.albumTag
+            + " '" + lyrics.album + "'."
+            + "</p>";
+    }
 
-    var script = "<speak>" + greeting + sayTime + songLibrary.lyrics[index].intro + songLibrary.lyrics[index].lyric + outro + "</speak>";
+    script +=
+        "<p>"
+        + lyrics.lyric
+        + "</p>"
+        + "<break time='2s'/>"
+        + phrases.outro
+        + "</speak>";
 
     var pollyParams = {
-        OutputFormat: 'mp3',
+        OutputFormat: "mp3",
         Text: script,
-        VoiceId: 'Brian',
+        VoiceId: phrases.voiceId,
         SampleRate: "16000",
-        TextType: 'ssml'
+        TextType: "ssml"
     };
 
     polly.synthesizeSpeech(pollyParams, function (err, data) {
         if (err) callback(err);
         else {
 
-            const mp3Key = "polly/" + uuidV4() + ".mp3";
+            const mp3Key = phrases.s3KeyPrefix + uuidV4() + ".mp3";
 
             var s3Params = {
-                Bucket: "aplx.us",
+                Bucket: phrases.s3Bucket,
                 Key: mp3Key,
-                //  Key : "polly/test.mp3",
                 ACL: "public-read",
                 Body: data.AudioStream,
                 ContentType: "audio/mpeg"
             };
 
-            s3.putObject(s3Params, function (err, data) {
+            s3.putObject(s3Params, function (err) {
                 if (err) callback(err);
                 else {
-                    var twilioInstruction = "https://s3.amazonaws.com/aplx.us/" + mp3Key;
+                    var twilioInstruction = "https://s3.amazonaws.com/" + phrases.s3Bucket + "/" + mp3Key;
                     callback(null, { "response": twilioInstruction });
                 }
-            })
+            });
         }
     });
 };
